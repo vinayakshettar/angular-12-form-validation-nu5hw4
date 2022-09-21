4886961615fcf06fb88443885fc3e9efdc7103e1
vinayakshettar <vinayakshettar@gmail.com>
2022-09-21 09:05:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-12-form-validation-xmz3os
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-12-form-validation-xmz3os)
---
+++ src/app/app.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import Validation from './utils/validation';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        fullname: ['', Validators.required],
+        username: [
+          null,
+          [
+            Validators.required,
+            Validators.pattern('^[+-]?[0-9]{1,2}(\\.[0-9]{1,3})?$')
+          ]
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue]
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')]
+      }
+    );
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
+}
